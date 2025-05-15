de296a195fe1e7cc14f47a24e910cc2c6b2d35f8
Si11-ibrahim <ahamedibrahim0004@gmail.com>
2025-05-16 03:06:44 +0530

    Updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1392053268"
+    serviceWorkerVersion: "191848041"
   }
 });
